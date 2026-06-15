ce15e664c6e6bcd86e627b96539888b6c543ef51
Harshit Singh <harshitsingh6405@gmail.com>
2026-06-16 02:20:42 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -2,7 +2,7 @@ num_1 = float(input("Enter the first number: "))
 abbrviation = input("Enter operation (+, -, *, /): ")
 num_2 = float(input("Enter the second number: "))
 
-if abbrviation not in ["+", "-", "*", "/"]:
+if abbrviation not in ["+", "-", "*", "/", "**"]:
     print("wrong abbreviation input")
     exit()
 
@@ -21,3 +21,7 @@ elif abbrviation == "/":
     else:
         result = num_1 / num_2
         print(f"The result of {num_1} / {num_2} is: {result:.3f}")
+elif abbrviation == "**":
+    result = num_1 ** num_2
+    print(f"The result of {num_1} ** {num_2} is: {result:.3f}")
+    
